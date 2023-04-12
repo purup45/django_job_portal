c6c32437e180768e65d8d475a08f1ac32390bee1
purup45 <mishraj436@gmail.com>
2023-04-12 09:00:18 -0400

    first commit

---
+++ job_portal/candidates/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Profile, Skill, AppliedJobs, SavedJobs
+
+admin.site.register(Profile)
+admin.site.register(Skill)
+admin.site.register(AppliedJobs)
+admin.site.register(SavedJobs)
